5986f2643c41bd26c27ac901df4101f15bee269b
Manish Ragella <manishragellaworkspace@gmail.com>
2026-05-18 14:02:50 +0530

    updated code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // No external image domains needed in demo mode
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
